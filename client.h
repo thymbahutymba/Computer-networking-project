e1aaab6c93286b84979419ec3a7d3506d160c3a9
Andrea <andrea.stevanato.95@hotmail.it>
2017-12-07 18:26:17 +0100

    remodel headers

--- client.h
+++ client.h
@@ -6,6 +6,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define CMD_SIZE 50
+#define BUFFER 1024
+
 void welcome(const char*,const char*,const char*);
 void stampacomandi();
 void split_command(const char*, char**);
+int register_user(char*, int, char*, char*);
+void who_command(int, char*);
+void put_command(int, char*);
+void send_username(int, char*);
--- server.h
+++ server.h
@@ -11,7 +11,6 @@
 
 struct users{
 	char* username;
-	struct sockaddr_in* user_addr;
 	struct info_sock* my_info;
 	struct msg_offline* first_msg;
 	struct users* next_user;
@@ -25,9 +24,16 @@ struct info_sock{
 struct msg_offline{
 	char* username;
 	char** msg;
+	int count;
 	struct msg_offline* next_msg;
 };
 
 void logging(char*);
 int register_username(int, struct users**, char**);
+void get_command(int, char*);
+void who_command(int, struct users*);
+void quit_command(int, struct users*);
+char* receive_username(int);
+void deregister_command(int, struct users**);
+
 
