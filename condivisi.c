68121d106201a479d700587924303eb26abf3913
Andrea <andrea.stevanato.95@hotmail.it>
2017-12-09 17:34:12 +0100

    fixed !quit command and add some function

--- condivisi.c
+++ condivisi.c
@@ -3,14 +3,14 @@
 void send_username(int sock, char* username){
 	
 	uint16_t lenght;
-	uint16_t lentc=strlen(username)+1;
+	uint16_t lentc=(username)?strlen(username)+1:0;
 
-	lenght = (username)?htons(lentc):htons(0);
+	lenght = htons(lentc);
 	if(send(sock, (void*)&lenght, sizeof(uint16_t), 0) < 0){
 		perror("Errore nell'invio della lunghezza dell'username");
 		exit(1);
 	}
-	if(ntohs(lenght)){
+	if(lentc){
 		if(send(sock,(void*)username, lentc, 0) < 0){
 			perror("Error nell'invio dell'username");
 			exit(1);
@@ -36,4 +36,44 @@ char* receive_username(int sock){
 	return username;
 }
 
+void send_uint(int sock, unsigned int valore){
+	unsigned int status=htons(valore);
+	
+	if(send(sock, &status, sizeof(status), 0) <0){
+		perror("Errore nell'invio dello status.");
+	}
+}
 
+unsigned int receive_uint(int sock){
+	unsigned int status;
+	
+	if(recv(sock, &status, sizeof(unsigned int), 0) <0)
+		perror("Errore nella ricezione dello status");
+	return ntohs(status);
+}
+
+void send_str(int sock, char* buffer){
+	unsigned int ltos, lenght;
+
+	ltos = strlen(buffer)+1;
+	lenght=htons(ltos);
+
+	if(send(sock, &lenght, sizeof(uint16_t), 0) <0)
+		perror("Errore nell'inviare la lunghezza");
+	if(send(sock, buffer, ltos, 0) <0)
+		perror("Errore nell'invio del messaggio");
+}
+
+char* receive_str(int sock){
+	unsigned int lenght;
+	char* buffer;
+
+	if(recv(sock, &lenght, sizeof(uint16_t), 0) <0)
+		perror("Errore nel ricevere la lunghezza del messaggio");
+
+	buffer=malloc(ntohs(lenght));
+	memset(buffer, 0, ntohs(lenght));
+	if(recv(sock, buffer, ntohs(lenght), 0) <0)
+		perror("Errore nel ricevere il messaggio");
+	return buffer;
+}
--- condivisi.h
+++ condivisi.h
@@ -5,3 +5,7 @@
 
 char* receive_username(int);
 void send_username(int, char*);
+unsigned int receive_uint(int);
+void send_uint(int, unsigned int);
+void send_str(int, char*);
+char* receive_str(int);
