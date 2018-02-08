774b1e70639902f580f2f2b77a12ce5e4f2d1536
Andrea <andrea.stevanato.95@hotmail.it>
2018-02-09 00:21:43 +0100

    3 to 4 panes

--- test.sh
+++ test.sh
@@ -2,5 +2,8 @@
 
 /bin/tmux new-session ./msg_server 8070 \; \
 	splitw -h ./msg_client 192.168.1.199 8075 192.168.1.199 8070 \; \
+	select-pane -t 0 \; \
 	splitw -v ./msg_client 192.168.1.199 8076 192.168.1.199 8070 \; \
+	select-pane -t 2 \; \
+	splitw -v ./msg_client 192.168.1.199 8077 192.168.1.199 8070 \; \
 	attach
