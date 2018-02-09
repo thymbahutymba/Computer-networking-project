fdc810c0f3a46e522bf18febc5e26db649fbfd79
Andrea <andrea.stevanato.95@hotmail.it>
2018-02-09 22:10:18 +0100

    moved definition function and structure from msg_client.c

--- client.h
+++ client.h
@@ -7,7 +7,14 @@
 #include <pthread.h>
 
 #define CMD_SIZE 50
-#define BUFFER 1024
+#define BUFFER_SIZE 1024
+
+struct thread_args{
+	char* ip;
+	char* port;
+	char* username;
+	int* UDP_sock;
+};
 
 void welcome(const char*,const char*,const char*);
 void stampacomandi();
@@ -16,5 +23,8 @@ void register_user(char*, int, char*, char*);
 void who_command(int, char*);
 void put_command(int, char*);
 void send_online(int, char*);
+void send_offline(int, char*);
+void receive_offmessage(int);
+void* receive_udp(void*);
 
 #endif
